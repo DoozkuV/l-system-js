d25b8f9d6bb86bce2650e71fefddd54f040f4efe
George Padron <georgenpadron@gmail.com>
2025-02-27 19:07:25 -0600

    Finished implementation of nodes

--- l-system.js
+++ l-system.js
@@ -1,33 +1,3 @@
-// A class to represent a node in the l-system
-class Node {
-	constructor(x, y, length, angle, depth) {
-		this.x = x;
-		this.y = y;
-		this.children = this.generateChildren();
-	}
-
-	generateChildren() {
-		if (this.depth === 0) return;
-
-		// Set the children's depth
-		const newDepth = depth - 1;
-
-		// Calculate coord for new branch-end
-		const endX = x + length * Math.cos(angle);
-		const endY = y + length * Math.sin(angle);
-
-		// Randomize branch splitting
-		const splitAngle = (Math.PI / 4) * (0.5 + random()); // Random angle between 22.5 and 67.5 degrees
-		const newLength = length * (0.6 + 0.2 * random()); // Random length reduction
-
-		// Recursively draw sub-branches
-
-		calcPoint(endX, endY, newLength, angle - splitAngle, newDepth);
-		calcPoint(endX, endY, newLength, angle + splitAngle, newDepth);
-
-	}
-}
-
 let seed = Math.random();
 
 function random() {
@@ -35,8 +5,45 @@ function random() {
 	return x - Math.floor(x);
 }
 
-function calcPoint(x, y, length, angle, depth) {
+// A class to represent a node in the l-system
+class Node {
+	#children
 
+	constructor(x, y, length, angle, depth) {
+		this.x = x;
+		this.y = y;
+		this.length = length;
+		this.angle = angle;
+		this.depth = depth;
+		this.#createChildren();
+	}
 
+	#createChildren() {
+		if (this.depth === 0) return;
 
+		// Set the children's depth
+		const newDepth = depth - 1;
+
+		// Calculate coord for new branch-end
+		const endX = x + this.length * Math.cos(angle);
+		const endY = y + this.length * Math.sin(angle);
+
+		// Randomize branch splitting
+		const splitAngle = (Math.PI / 4) * (0.5 + random()); // Random angle between 22.5 and 67.5 degrees
+		const newLength = this.length * (0.6 + 0.2 * random()); // Random length reduction
+
+		// Set children to subbranches
+		this.#children = {
+			left: new Node(endX, endY, newLength, angle - splitAngle, newDepth),
+			right: new Node(endX, endY, newLength, angle + splitAngle, newDepth)
+		};
+	}
+
+	hasChildren() {
+		return this.#children !== undefined;
+	}
+
+	get children() {
+		return this.#children;
+	}
 }
