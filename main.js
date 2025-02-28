ed5f60b93485b0df97dfcb4c301aece632156d2c
George Padron <georgenpadron@gmail.com>
2025-02-27 20:31:09 -0600

    Added depth slider

--- main.js
+++ main.js
@@ -2,19 +2,27 @@ import { Root } from "./l-system.js";
 import { renderTree } from "./canvas-renderer.js";
 
 const canvas = document.getElementById('treeCanvas');
+const renderButton = document.getElementById("renderButton");
+const depthSlider = document.getElementById("depthSlider");
+const depthValue = document.getElementById("depthValue");
 
 const trunkLength = 100;
-const depth = 8;
+let depth = 8;
 const rootX = canvas.width / 2; // Middle of the canvas
 const rootY = canvas.height; // Bottom of the canvas
-let root = new Root(rootX, rootY, trunkLength, depth);
+const root = new Root(rootX, rootY, trunkLength, depth);
 
 renderTree(root);
 
-const renderButton = document.getElementById("renderButton");
+const reRenderTree = () => {
+		const root = new Root(rootX, rootY, trunkLength, depth);
+				renderTree(root);
+}
 
-renderButton.addEventListener("click", () => {
-		let root = new Root(rootX, rootY, trunkLength, depth);
-		
-		renderTree(root);
+renderButton.addEventListener("click", reRenderTree);
+canvas.addEventListener("click", reRenderTree);
+
+depthSlider.addEventListener("input", () => {
+		depth = parseInt(depthSlider.value);
+		depthValue.textContent = depth;
 });
