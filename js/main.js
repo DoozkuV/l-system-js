e2329c283fa6ea53ab519e43a9293fe87ce4c13c
George Padron <georgenpadron@gmail.com>
2025-03-11 21:01:00 -0500

    Changed ordering of main file

--- js/main.js
+++ js/main.js
@@ -1,27 +1,25 @@
 import { Root } from "./l-system.js";
 import { renderTree } from "./canvas-renderer.js";
 
+// Prepare UI elements 
 const canvas = document.getElementById('treeCanvas');
+const renderButton = document.getElementById("renderButton");
+const depthSlider = document.getElementById("depthSlider");
+const depthValue = document.getElementById("depthValue");
 
 // Initialize the tree
 let depth = 8;
 const trunkLength = 100;
 const rootX = canvas.width / 2; // Middle of the canvas
 const rootY = canvas.height; // Bottom of the canvas
-const root = new Root(rootX, rootY, trunkLength, depth);
 
-// Load the tree when ready 
-window.addEventListener("load", () => renderTree(root));
-
-// UI FUNCTIONALITY
-const renderButton = document.getElementById("renderButton");
-const depthSlider = document.getElementById("depthSlider");
-const depthValue = document.getElementById("depthValue");
-
+// Function for rendering the tree
 const createNewTree = () => {
 	const root = new Root(rootX, rootY, trunkLength, depth);
 	renderTree(root);
 }
+// Render the tree on load and on button
+window.addEventListener("load", createNewTree);
 renderButton.addEventListener("click", createNewTree);
 canvas.addEventListener("click", createNewTree);
 
